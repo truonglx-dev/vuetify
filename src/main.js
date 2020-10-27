283c2c05c4639c96182d16747e6f5feb3252a4b7
truonglx-dev <xuantruong.220494@gmail.com>
2020-10-28 00:32:03 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-
-Vue.config.productionTip = false;
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import './styles/style.scss'
+Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <v-row justify="space-between" class="pa-3">
+      <v-btn text> <v-icon>mdi-chevron-left</v-icon>Back </v-btn>
+      <v-btn text width="auto">
+        <v-icon>
+          mdi-chevron-right
+        </v-icon>
+      </v-btn>
+    </v-row>
+    <v-row class="pt-4 d-flex flex-column text-center block-title">
+      <div class="title pb-4">Card Type</div>
+      <div class="sub-title">
+        Choose the type of card to add PayInde
+      </div>
+    </v-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
-};
+}
 </script>
+
+<style scoped lang="scss">
+.block-title {
+  .title {
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 16px;
+  }
+  .sub-title {
+    font-size: 14px;
+    line-height: 16px;
+  }
+}
+</style>
